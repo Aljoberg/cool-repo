17819cd12247ef15b91ee207e6dfcadbbfeb0f00
Pain6900 <97961226+Pain6900@users.noreply.github.com>
2022-04-30 15:37:34 +0530

    Update announce.js

--- others/announce.js
+++ others/announce.js
@@ -22,7 +22,7 @@ module.exports = {
       .setTitle("Ayo wassup?")
       .setDescription(`Invitation Links To Bots Created By:`)
       .addField("• DEV", `\`\`\`yml\nName of Dev: 【亗『PAIN』亗】#4005 \`\`\``)      
-      .addField("• Important Links", `**\`[Support Server](https://discord.gg/RNcbXMGBJh)\`|\`[Insta](https://www.instagram.com/akt_pain69/)\**`)
+      .addField("• Important Links", `**[Support Server](https://discord.gg/RNcbXMGBJh)\|\[Insta](https://www.instagram.com/akt_pain69/)\**`)
       .addField("• Invite To Server", `**[【💔 ✘ ᴘᴀɪɴ 】]( https://discord.com/api/oauth2/authorize?client_id=968792641137430538&permissions=8&scope=bot )\`|\`[【⭕ ✘ ᴘᴀɪɴ 】](https://discord.com/api/oauth2/authorize?client_id=967514923229933588&permissions=8&scope=bot)\`|\`[【❌ ✘ ᴘᴀɪɴ 】](https://discord.com/api/oauth2/authorize?client_id=968257236013056122&permissions=8&scope=bot)\**`)     
       .addField("Plz Leave A Review In The Support Server",`>> Mention @亗『PAIN』亗】#4005 In Support Server For Any Help <3 `)   
       .setTimestamp()
