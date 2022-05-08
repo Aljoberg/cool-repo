59fabd880e4b180ebd08dcfe6d28302282d376cb
PAIN <97961226+Pain6900@users.noreply.github.com>
2022-05-08 21:34:38 +0530

    Create meme.js

---
+++ others/meme.js
@@ -0,0 +1,32 @@
+const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const {
+  PREFIX,
+  approveemoji,
+  denyemoji
+} = require(`../config.json`);
+const fetch = require("node-fetch");
+
+
+
+module.exports = {
+  name: "meme",
+  description: "This command sends you random memes",
+  aliases: ["random meme"],
+  cooldown: 2,
+  edesc: "Sends random meme",
+  async execute(message, args, client) {
+    //react with approve emoji
+    message.react("✅");
+   
+    var url = await fetch("https://www.reddit.com/r/memes/random/.json");
+    console.log(url);
+    var random = await url.json();
+
+    //send the Hi embed
+        message.reply(new MessageEmbed().setColor("#FF00A6").setTitle(`Random Meme | ${random[0].data.children[0].data.title}`)
+.setImage(random[0].data.children[0].data.url)        
+        .setFooter(client.user.username, client.user.displayAvatarURL())
+    .setTimestamp()
+                     )
+    }
+};
